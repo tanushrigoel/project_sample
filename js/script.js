08db35d34ab4f4641309c204500f727bbdde7a8c
Prince Keshri <pr.prince.3068@gmail.com>
2025-04-18 22:17:38 +0530

    partner page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
 
 // Start here
+// Basic JavaScript for any interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+
+    ctaButton.addEventListener('click', function() {
+        alert('Thank you for your interest! Free trial will begin shortly.');
+    });
+
+    // You could add animations or other interactivity here
+});
